13c54ed14452fb3ec6f054265498ab79e332ca10
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2024-11-20 15:03:35 +0000

    DVOP-3032 Bumping version to 1.0.294

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,8 +19,7 @@ terraform {
 }
 
 module "ecs-service" {
-#  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.258"
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.292"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.294"
 
   # Environmental configuration
   environment             = var.environment
